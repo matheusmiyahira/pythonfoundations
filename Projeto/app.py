c379ccd81ef2af2c380aec61c1256ef44cbc0fc9
matheusmiyhira <matheusmiyahira@gmail.com>
2019-12-06 22:02:19 -0300

    4

--- Projeto/app.py
+++ Projeto/app.py
@@ -2,11 +2,13 @@ import flask
 
 from blueprints.docker_blueprint import docker_routes
 from blueprints.jenkins_blueprint import jenkins_routes
+from blueprints.ldap_blueprint import ldap_routes
 
 app = flask.Flask(__name__)
 
 app.register_blueprint(docker_routes)
 app.register_blueprint(jenkins_routes)
+app.register_blueprint(ldap_routes)
 
 
 @app.route('/')
