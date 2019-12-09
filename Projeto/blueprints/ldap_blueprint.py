1c4c9b7299fa59300589debc76a84147a31599bd
matheusmiyhira <matheusmiyahira@gmail.com>
2019-12-09 19:16:05 -0300

    1

--- Projeto/blueprints/ldap_blueprint.py
+++ Projeto/blueprints/ldap_blueprint.py
@@ -1,12 +1,22 @@
 import flask
 import ldap3
 
-ldap_routes = flask.Blueprint(name='ldap',import_name='__name__',url_prefix='/ldap')
+ldap_routes = flask.Blueprint(name='ldap',import_name='__name__',url_prefix='/login')
 
 @ldap_routes.route('/')
 def index():
     return flask.render_template('login.jinja')
 
-@ldap_routes.route('/login',methods=['POST'])
+@ldap_routes.route('/',methods=['POST'])
 def login():
-    return flask.request.form
+    usuario = flask.request.form['email']
+    senha = flask.request.form['password']
+    try:
+        server = ldap3.server('ldap://localhost:389')
+        client = ldap3.Connection(server, f'cn={usuario},dc=example,dc=org, senha')
+        conectou = client.bind()
+    except expression as identifier:
+        print('Não foi possível conectar com o servidor.')
+        conectou = 'Não foi possível conectar'
+
+    return conectou
